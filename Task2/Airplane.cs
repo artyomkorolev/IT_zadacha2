3bffa3f8569af8b93a8c1f0a03f3edb4b2c83875
artyomkorolev <Artyom.King.03@gmail.com>
2024-06-01 17:24:54 +0300

    Добавьте файлы проекта.

---
+++ Task2/Airplane.cs
@@ -0,0 +1,26 @@
+﻿namespace Task2;
+public class Airplane : Aircraft
+{
+    public double RunwayLength { get; set; }
+    
+    public override bool TakeOff()
+    {
+        if (RunwayLength >= 1000)
+        {
+            DisplayMessage("Самолет взлетел.");
+            return true;
+        }
+        else
+        {
+            DisplayMessage("Взлеточная полоса слишком короткая.");
+            return false;
+        }
+    }
+    
+    public override bool Land()
+    {
+        DisplayMessage("Самолет приземлился.");
+        return true;
+    }
+}
+
---
+++ Task2/Helicopter.cs
@@ -0,0 +1,18 @@
+﻿namespace Task2;
+
+public class Helicopter : Aircraft
+{
+    public override bool TakeOff()
+    {
+        DisplayMessage("Вертолет взлетел.");
+        return true;
+    }
+    
+    public override bool Land()
+    {
+        DisplayMessage("Вертолет приземлился.");
+        return true;
+    }
+}
+
+
